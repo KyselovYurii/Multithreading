4cbe8a04e10f57c5388a44beb930b0d40781f6c2
KyselovYurii <kyselov.yurii@gmail.com>
2021-11-18 15:54:09 +0200

    Task5: Create two threads and a shared collection

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,18 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        private static readonly List<int> _sharedCollection = new List<int>();
+        private static readonly AutoResetEvent _printEvent = new AutoResetEvent(true);
+        private static readonly AutoResetEvent _addEvent = new AutoResetEvent(false);
+        private static readonly CancellationTokenSource cts = new CancellationTokenSource();
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +24,35 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var addThread = new Thread(AddItems);
+            var printThread = new Thread(PrintItems);
+            addThread.Start();
+            printThread.Start();
+
+            addThread.Join();
 
             Console.ReadLine();
+        }
+        private static void AddItems()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _printEvent.WaitOne();
+                _sharedCollection.Add(i);
+                _addEvent.Set();
+            }
+
+            cts.Cancel();
+        }
+
+        private static void PrintItems()
+        {
+            while (!cts.Token.IsCancellationRequested)
+            {
+                _addEvent.WaitOne();
+                Console.WriteLine($"[{string.Join(", ", _sharedCollection)}]");
+                _printEvent.Set();
+            }
         }
     }
 }
